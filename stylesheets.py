a7ace855b639d9d004b0788c7b62d8a748a8ffc3
haydena23 <haydena23@up.edu>
2023-12-31 04:54:10 -0800

    More dark mode, remove dark age database

--- stylesheets.py
+++ stylesheets.py
@@ -1,11 +1,36 @@
 dark_mode_style = """
-    QMainWindow, QDialog, QWidget {
-        background-color: #2d2d2d; /* Dark background color */
-        color: #d3d3d3; /* Light text color */
+    QMenuBar, QMenu {
+        background-color: #2b2f38;
+        color: #9da5b4;
     }
-    QLabel, QPushButton, QLineEdit, QTableWidget, QComboBox {
-        background-color: #454545; /* Darker elements */
-        color: #ffffff;
-        border: 1px solid #767676;
+    QMenuBar::item:selected , QMenu::item:selected {
+        background-color: #313438;
+    }
+    QMainWindow {
+        background-color: #2b2f38; /* Dark background color */
+        color: #abb2bf; /* Light text color */
+    }
+    QLabel, QGroupBox {
+        color: #abb2bf;
+    }
+    QTabWidget::pane {
+        background-color: #494d52;
+    }
+    QTabBar::tab {
+        background-color: #21252b;
+        color: #9da5b4;
+    }
+    QTabBar::tab:selected {
+        background-color: #2b2f38;
+        color: #9da5b4;
+    }
+    QTableWidget, QTableWidget::Section {
+        background-color: #585a5c;
     }
 """
+
+    # QLabel, QPushButton, QLineEdit, QTableWidget, QComboBox {
+    #     background-color: #454545; /* Darker elements */
+    #     color: #ffffff;
+    #     border: 1px solid #767676;
+    # # }
